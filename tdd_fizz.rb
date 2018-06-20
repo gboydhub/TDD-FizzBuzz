f70f549e1390e571d4bdf8b5d23c13934f04b376
Gary Boyd <eponick@gmail.com>
2018-06-20 13:23:01 -0400

    every fifteenth element returns mined minds, pass

--- tdd_fizz.rb
+++ tdd_fizz.rb
@@ -1,12 +1,13 @@
 def fizzbuzz()
     arr = [*1..100]
     arr.each_with_index do |v, i|
-        if v%3 == 0
+        if v%15 == 0
+            arr[i] = "mined minds"
+        elsif v%3 == 0
             arr[i] = "mined"
         elsif v%5 == 0
             arr[i] = "minds"
         end
     end
-    arr[14] = "mined minds"
     arr
 end

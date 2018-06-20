303ce0bc6152fa8652253e7b4a9df9d4f0c33cdd
Gary Boyd <eponick@gmail.com>
2018-06-20 11:49:40 -0400

    test result is array, fail

--- test_tdd_fizz.rb
+++ test_tdd_fizz.rb
@@ -5,8 +5,9 @@ class TestTddFizz < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
+    end 
+
+    def test_result_is_array
+        assert_equal(Array, fizzbuzz().class)
     end
-
-    
-
 end

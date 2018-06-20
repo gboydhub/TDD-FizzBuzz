49325083f91fd5e952d7d250870b2fdcfadcf839
Gary Boyd <eponick@gmail.com>
2018-06-20 11:39:47 -0400

    Fixed class

--- test_tdd_fizz.rb
+++ test_tdd_fizz.rb
@@ -1,10 +1,12 @@
 require "minitest/autorun"
 require_relative "tdd_fizz.rb"
 
-class TestEvenOdd < Minitest::Test
+class TestTddFizz < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+
+    
 
 end
